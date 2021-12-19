b82a5ad9aa846d11f3bd80ebe2c2c40a42b6d199
Ivan <i.rinaldi4@studenti.uniba.it>
2021-12-19 18:05:41 +0100

    Creati metodi per gestione Visita offline

--- app/src/main/java/it/uniba/pioneers/data/Visita.java
+++ app/src/main/java/it/uniba/pioneers/data/Visita.java
@@ -2,6 +2,7 @@ package it.uniba.pioneers.data;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.widget.Toast;
 
@@ -165,6 +166,77 @@ public class Visita {
 
     }
 
+    //Crea record dal DB in base ai dati forniti
+    public void createDataDb(Context context){
+        if(isOnline()){
+            RequestQueue queue = Volley.newRequestQueue(context);
+            String url = Server.getUrl() + "/visita/create/";
+
+            JSONObject data = new JSONObject();
+            try {
+
+                if( getCreatore_visitatore() != 0  ){
+                    data.put(DbContract.VisitaEntry.COLUMN_CREATORE_VISITATORE, getCreatore_visitatore());
+                }
+                if( getCreatore_curatore() != 0  ){
+                    data.put(DbContract.VisitaEntry.COLUMN_CREATORE_CURATORE, getCreatore_curatore());
+                }
+                if( getGuida() != 0  ){
+                    data.put(DbContract.VisitaEntry.COLUMN_GUIDA, getGuida());
+                }
+                data.put(DbContract.VisitaEntry.COLUMN_DATA, getData());
+                data.put(DbContract.VisitaEntry.COLUMN_TIPO_CREATORE, getTipo_creatore());
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            Visita self = this;
+
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, data,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            try {
+                                Boolean status =  response.getBoolean("status");
+                                if(status){
+                                    self.setDataFromJSON(response.getJSONObject("data"));
+                                    Toast.makeText(context, response.toString(), Toast.LENGTH_SHORT).show();
+                                }else{
+                                    Toast.makeText(context, "Non è avenuto nessun cambio dati, verifica che i valori siano validi", Toast.LENGTH_SHORT).show();
+                                }
+                            } catch (JSONException | ParseException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Toast.makeText(context, "Il server non risponde", Toast.LENGTH_SHORT).show();
+                    System.out.println(error.toString());
+                }
+            });
+            queue.add(jsonObjectRequest);
+        }else{
+            //SQLITE3 Necessario per gestire le richieste offline
+
+            //Creazione oggetto di tipo dbHelper e lo istanzio come un WritableDatabase
+            DbHelper dbHelper = new DbHelper(context);
+            SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+            //Imposto i valori dell'istanza da creare all'interno del db
+            ContentValues values = new ContentValues();
+            values.put(DbContract.VisitaEntry.COLUMN_CREATORE_VISITATORE, getCreatore_visitatore());
+            values.put(DbContract.VisitaEntry.COLUMN_GUIDA, getGuida());
+            values.put(DbContract.VisitaEntry.COLUMN_CREATORE_CURATORE, getCreatore_curatore());
+            values.put(DbContract.VisitaEntry.COLUMN_DATA, getData());
+            values.put(DbContract.VisitaEntry.COLUMN_TIPO_CREATORE, getTipo_creatore());
+
+            //Inserisco i valori dell'istanza da creare all'interno del db
+            long newRowId = db.insert(DbContract.ZonaEntry.TABLE_NAME, null, values);
+        }
+    }
+
     //Legge record dal DB in base all'id fornito
     public void readDataDb(Context context){
         if( isOnline() ){
@@ -207,7 +279,89 @@ public class Visita {
             });
             queue.add(jsonObjectRequest);
         }else{
-            //TODO SQLITE3
+            //SQLITE3 Necessario per gestire le richieste offline
+
+            //Creazione oggetto di tipo dbHelper e lo istanzio come un ReadableDatabase
+            DbHelper dbHelper = new DbHelper(context);
+            SQLiteDatabase db = dbHelper.getReadableDatabase();
+
+            //Definiamo quali colonne dovranno essere presenti all'interno della proiezione
+            String[] projection = {
+                DbContract.VisitaEntry.COLUMN_ID,
+                DbContract.VisitaEntry.COLUMN_CREATORE_VISITATORE,
+                DbContract.VisitaEntry.COLUMN_GUIDA,
+                DbContract.VisitaEntry.COLUMN_CREATORE_CURATORE,
+                DbContract.VisitaEntry.COLUMN_DATA,
+                DbContract.VisitaEntry.COLUMN_TIPO_CREATORE
+            };
+
+            //Troviamo la riga all'interno del db e ricaviamo i valori
+            String selection = DbContract.VisitaEntry.COLUMN_ID + " = ?";
+            String[] selectionArgs = { String.valueOf(getId()) };
+
+            //Impostiamo l'ordine che dovranno seguire i risultati
+            String sortOrder = DbContract.VisitaEntry.COLUMN_ID + " DESC";
+
+            //Effettuiamo la query che ci restituirà i valori richiesti
+            Cursor c = db.query(
+                    DbContract.VisitaEntry.TABLE_NAME,
+                    projection,
+                    selection,
+                    selectionArgs,
+                    null,
+                    null,
+                    sortOrder
+            );
+
+            //Spostiamo l'oggetto Cursor c alla prima riga presente all'interno del RecordSet
+            c.moveToNext();
+
+            //Salvo i vari dati presenti in tale riga
+            int id = c.getInt(
+                    c.getColumnIndexOrThrow(
+                            DbContract.VisitaEntry.COLUMN_ID
+                    )
+            );
+            setId(id);
+
+            int creatore_visitatore = c.getInt(
+                    c.getColumnIndexOrThrow(
+                            DbContract.VisitaEntry.COLUMN_CREATORE_VISITATORE
+                    )
+            );
+            setCreatore_visitatore(creatore_visitatore);
+
+            int cognome = c.getInt(
+                    c.getColumnIndexOrThrow(
+                            DbContract.VisitaEntry.COLUMN_GUIDA
+                    )
+            );
+            setGuida(cognome);
+
+            int creatore_curatore = c.getInt(
+                    c.getColumnIndexOrThrow(
+                            DbContract.VisitaEntry.COLUMN_CREATORE_CURATORE
+                    )
+            );
+            setCreatore_curatore(creatore_curatore);
+
+            long data = c.getLong(
+                    c.getColumnIndexOrThrow(
+                            DbContract.VisitaEntry.COLUMN_DATA
+                    )
+            );
+            setData(data);
+
+            int tipo_creatore = c.getInt(
+                    c.getColumnIndexOrThrow(
+                            DbContract.VisitaEntry.COLUMN_TIPO_CREATORE
+                    )
+            );
+            setTipo_creatore(tipo_creatore);
+
+            //Chiusura del Cursor c poichè non più necessario
+            c.close();
+
         }
     }
 
@@ -264,66 +418,11 @@ public class Visita {
             });
             queue.add(jsonObjectRequest);
         }else{
-            //TODO SQLITE3
-        }
-    }
-
-    //Crea record dal DB in base ai dati forniti
-    public void createDataDb(Context context){
-        if(isOnline()){
-            RequestQueue queue = Volley.newRequestQueue(context);
-            String url = Server.getUrl() + "/visita/create/";
-
-            JSONObject data = new JSONObject();
-            try {
-
-                if( getCreatore_visitatore() != 0  ){
-                    data.put(DbContract.VisitaEntry.COLUMN_CREATORE_VISITATORE, getCreatore_visitatore());
-                }
-                if( getCreatore_curatore() != 0  ){
-                    data.put(DbContract.VisitaEntry.COLUMN_CREATORE_CURATORE, getCreatore_curatore());
-                }
-                if( getGuida() != 0  ){
-                    data.put(DbContract.VisitaEntry.COLUMN_GUIDA, getGuida());
-                }
-                data.put(DbContract.VisitaEntry.COLUMN_DATA, getData());
-                data.put(DbContract.VisitaEntry.COLUMN_TIPO_CREATORE, getTipo_creatore());
-
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-
-            Visita self = this;
-
-            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, data,
-                    new Response.Listener<JSONObject>() {
-                        @Override
-                        public void onResponse(JSONObject response) {
-                            try {
-                                Boolean status =  response.getBoolean("status");
-                                if(status){
-                                    self.setDataFromJSON(response.getJSONObject("data"));
-                                    Toast.makeText(context, response.toString(), Toast.LENGTH_SHORT).show();
-                                }else{
-                                    Toast.makeText(context, "Non è avenuto nessun cambio dati, verifica che i valori siano validi", Toast.LENGTH_SHORT).show();
-                                }
-                            } catch (JSONException | ParseException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                    }, new Response.ErrorListener() {
-                @Override
-                public void onErrorResponse(VolleyError error) {
-                    Toast.makeText(context, "Il server non risponde", Toast.LENGTH_SHORT).show();
-                    System.out.println(error.toString());
-                }
-            });
-            queue.add(jsonObjectRequest);
-        }else{
             //SQLITE3 Necessario per gestire le richieste offline
             DbHelper dbHelper = new DbHelper(context);
             SQLiteDatabase db = dbHelper.getWritableDatabase();
 
+            //Imposto i nuovi valori da passare al db
             ContentValues values = new ContentValues();
             values.put(DbContract.VisitaEntry.COLUMN_CREATORE_VISITATORE, getCreatore_visitatore());
             values.put(DbContract.VisitaEntry.COLUMN_GUIDA, getGuida());
@@ -331,7 +430,18 @@ public class Visita {
             values.put(DbContract.VisitaEntry.COLUMN_DATA, getData());
             values.put(DbContract.VisitaEntry.COLUMN_TIPO_CREATORE, getTipo_creatore());
 
-            long newRowId = db.insert(DbContract.ZonaEntry.TABLE_NAME, null, values);
+            //Trovo la riga da aggiornare all'interno del db
+            String selection = DbContract.VisitaEntry.COLUMN_ID + " = ?";
+            String[] selectionArgs = { String.valueOf(getId()) };
+
+            //Eseguo l'update per tale riga con i nuovi valori presenti all'interno di values
+            int c = db.update(
+                    DbContract.VisitaEntry.TABLE_NAME,
+                    values,
+                    selection,
+                    selectionArgs
+            );
+
         }
     }
 
@@ -376,7 +486,17 @@ public class Visita {
             });
             queue.add(jsonObjectRequest);
         }else{
-            //TODO SQLITE3
+            //SQLITE3 Necessario per gestire le richieste offline
+            DbHelper dbHelper = new DbHelper(context);
+            SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+            //Selezione la riga da eliminare
+            String selection = DbContract.VisitaEntry.COLUMN_ID + " = ?";
+            String[] selectionArgs = { String.valueOf(getId()) };
+
+            //Elimino la riga all'interno del db dopo la selection
+            int deleteRows = db.delete(DbContract.VisitaEntry.TABLE_NAME,selection,selectionArgs);
+
         }
     }
     
