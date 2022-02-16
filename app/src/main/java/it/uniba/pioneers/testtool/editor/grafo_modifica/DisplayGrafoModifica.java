370bbeaf10aa0cd245b76a7565e9895dbf94dcad
root-hunter <a.ricciardi27@studenti.uniba.it>
2022-02-16 18:42:53 +0100

    Modifiche per sull'interfaccia del grafo modifica

--- app/src/main/java/it/uniba/pioneers/testtool/editor/grafo_modifica/DisplayGrafoModifica.java
+++ app/src/main/java/it/uniba/pioneers/testtool/editor/grafo_modifica/DisplayGrafoModifica.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import androidx.annotation.NonNull;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import it.uniba.pioneers.data.Visita;
 import it.uniba.pioneers.testtool.editor.listaNodi.ListaNodi;
 
 public class DisplayGrafoModifica extends ConstraintLayout {
@@ -12,18 +13,20 @@ public class DisplayGrafoModifica extends ConstraintLayout {
 
     DisplayGrafoModifica self = null;
     Context context = null;
+    Visita visita = null;
 
     public void init(){
         self = this;
-        graph = new GrafoModifica(context);
+        graph = new GrafoModifica(context, visita);
         graph.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
 
         addView(graph); //AGGIUNTA DEL GRAFO AL DISPLAYGRAFO
     }
 
 
-    public DisplayGrafoModifica(@NonNull Context context) {
+    public DisplayGrafoModifica(@NonNull Context context, Visita visita) {
         super(context);
+        this.visita = visita;
         this.context = context;
         init();
     }
