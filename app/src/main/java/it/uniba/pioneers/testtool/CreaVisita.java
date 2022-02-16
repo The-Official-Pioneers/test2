e4dea97e1f8809627a8ae132213ffdbda894ac89
Ivan <i.rinaldi4@studenti.uniba.it>
2022-02-16 18:44:49 +0100

    Fix Visita.java: modifica al metodo CreateDataDb e inizio collegamento CreaVisita.java con Editor

--- app/src/main/java/it/uniba/pioneers/testtool/CreaVisita.java
+++ app/src/main/java/it/uniba/pioneers/testtool/CreaVisita.java
@@ -5,21 +5,29 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.Response;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.text.ParseException;
+import java.time.Instant;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
+import it.uniba.pioneers.data.Visita;
 import it.uniba.pioneers.data.Zona;
 
 public class CreaVisita extends AppCompatActivity {
 
     private String selectedItem = "";
+    public static Visita visita;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,7 +42,7 @@ public class CreaVisita extends AppCompatActivity {
         setLuoghiVisite(this);
     }
 
-    //Gestine Spinner con Luoghi presenti nel DB
+    //Gestione Spinner con Luoghi presenti nel DB
     public void setLuoghiVisite(Context context){
         Spinner spinner = (Spinner) this.findViewById(R.id.spinner_scegli_luogo);
 
@@ -76,8 +84,40 @@ public class CreaVisita extends AppCompatActivity {
 
 
     public void goToEditorAfterLuogo(View view) {
-        Spinner spinner = (Spinner) this.findViewById(R.id.spinner_scegli_luogo);
-        selectedItem = spinner.getSelectedItem().toString();
+
+        visita = new Visita();
+
+        if(MainActivity.tipoUtente.equals("visitatore")){
+
+            Spinner spinner = (Spinner) this.findViewById(R.id.spinner_scegli_luogo);
+            selectedItem = spinner.getSelectedItem().toString();
+
+            visita.setTipo_creatore(1);
+            visita.setCreatore_visitatore(Math.toIntExact(MainActivity.visitatore.getId()));
+            Date tmpDate = Date.from(Instant.now());
+            Long tmpLong = tmpDate.getTime();
+            visita.setData(tmpLong);
+            visita.setLuogo(selectedItem);
+            visita.createDataDb(this, new Response.Listener<JSONObject>() {
+                @Override
+                public void onResponse(JSONObject response) {
+                    try {
+                        Boolean status =  response.getBoolean("status");
+                        if(status){
+                            visita.setDataFromJSON(response.getJSONObject("data"));
+                            System.out.println("ID VISITA" + visita.getId());
+                        }else{
+                            Toast.makeText(getApplicationContext(), R.string.cambio_dati_no_validi, Toast.LENGTH_SHORT).show();
+                        }
+                    } catch (JSONException | ParseException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+
+        } else if(MainActivity.tipoUtente.equals("curatore")){
+
+        }
 
         /* PER ANTONIO
         * da dichiarare come variabile nella classe
